8d1e1e02ef0fa44ce61dd8ef4ccf323d34812f84
pragati <itispragativerma@gmail.com>
2020-04-02 00:19:18 +0530

    added models

--- snippet_share_project/snip_app/models.py
+++ snippet_share_project/snip_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django import forms
 
-# Create your models here.
+class Snip(models.Model):
+    alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    title=models.CharField(max_length=40, default="Untitled")
+    text=models.TextField()
+    link_code=models.CharField(max_length=8,unique=True, validators=[alphanumeric])
+    langs=[('text','None'),('c','C'),('cpp','C++'),('csharp','C#'),('java','Java'),('js','JavaScript'),('php','PHP'),('py','Python'),('sql','SQL')]
+    lang=models.CharField(max_length=10, choices=langs, default='text')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.link_code+" : "+self.text[:20]
--- snippet_share_project/snippet_share/settings.py
+++ snippet_share_project/snippet_share/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'snip_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'snippet_share.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['snippet_share/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'snippet_share/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
