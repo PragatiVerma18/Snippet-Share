484d9f4cabcb21d3b7fc2f4e1ef7d8d357321585
pragati <itispragativerma@gmail.com>
2020-04-08 00:57:49 +0530

    deploy files added

--- snippet_share_project/snippet_share/settings.py
+++ snippet_share_project/snippet_share/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +132,4 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+django_heroku.settings(locals())
